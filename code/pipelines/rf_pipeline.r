08d3b78c543f12bf8595caae72e50815bd0ada8a
jlinaresb <joselb1993@gmail.com>
2022-10-10 11:29:35 +0200

    add randomForest

--- code/pipelines/rf_pipeline.r
+++ code/pipelines/rf_pipeline.r
@@ -1,9 +1,10 @@
-# RandomForest function
+# Glmnet function
 # =======
 setwd(here::here())
 source("code/configFile.r")
 source("code/utils/build_learners.r")
 source("code/utils/tuners.r")
+source("code/utils/pipeline_utils.r")
 
 rf_pipeline <- function(data,
                         dataname,
@@ -12,44 +13,29 @@ rf_pipeline <- function(data,
                         removeConstant,
                         normalize,
                         filterFeatures,
-                        outDir){
-
-  data <- as.data.frame(data)
-  data[, target] <- as.factor(data[, target])
+                        method,
+                        measure,
+                        nevals,
+                        outDir) {
   # Make task
-  task <- TaskClassif$new(id = dataname,
-                          backend = data,
-                          target = target,
-                          positive = positive)
-  # Remove constant features
-  if (removeConstant == TRUE) {
-    print("Removing constant features")
-    rcf <- po("removeconstants")
-    task <- rcf$train(list(task = task))$output
-  }
-  # Normalizing features
-  if (normalize == TRUE) {
-    print("Normalizing features")
-    nf <- po("scale")
-    task <- nf$train(input = list(task))$output
-  }
-  # Filter features
-  if (filterFeatures == TRUE) {
-    print("Filtering features")
-    filter <- po("filter", filter = flt("kruskal_test"), filter.frac = 0.3)
-    task <- filter$train(list(task = task))$output
-  }
+  task <- making_task(data,
+                      dataname,
+                      target,
+                      positive)
+  # Preprocess
+  task <- preprocess(task,
+                     removeConstant,
+                     normalize,
+                     filterFeatures)
   # Learner
   learner <- randomForest()
   # Nested resampling
-  rr <- fselect_nested(
-    method = "genetic_search",
-    task = task,
-    learner = learner,
-    inner_resampling = rsmp("holdout"),
-    outer_resampling = rsmp("cv", folds = 10),
-    measure = msr("classif.ce"),
-    term_evals = 100
-  )
-  saveRDS(rr, file = paste0(outDir, "rsmp_rf_", dataname, ".rds"))
+  rr <- nested_resampling(task,
+                          learner,
+                          method,
+                          nevals,
+                          measure)
+  # Save resampling object
+  saveRDS(rr,
+          file = paste0(outDir, "rsmp_randomForest_", dataname, ".rds"))
 }
---
+++ code/utils/pipeline_utils.r
@@ -0,0 +1,61 @@
+# Making task
+making_task <- function(data, dataname, target, positive) {
+    data <- as.data.frame(data)
+    data[, target] <- as.factor(data[, target])
+    # Make task
+    task <- TaskClassif$new(id = dataname,
+                        backend = data,
+                        target = target,
+                        positive = positive)
+    return(task)
+}
+
+# Preprocess
+preprocess <- function(task,
+                       removeConstant,
+                       normalize,
+                       filterFeatures) {
+
+  # Remove constant features
+  if (removeConstant == TRUE) {
+    print("Removing constant features")
+    rcf <- po("removeconstants")
+    task <- rcf$train(list(task = task))$output
+  }
+  # Normalizing features
+  if (normalize == TRUE) {
+    print("Normalizing features")
+    nf <- po("scale")
+    task <- nf$train(input = list(task))$output
+  }
+  # Filter features
+  if (filterFeatures == TRUE) {
+    print("Filtering features")
+    filter <- po("filter",
+                 filter = flt("kruskal_test"),
+                 filter.frac = 0.3)
+    task <- filter$train(list(task = task))$output
+  }
+
+  return(task)
+}
+
+
+# Nested resampling
+nested_resampling <- function(task,
+                              learner,
+                              method,
+                              nevals = 100,
+                              measure = "classif.ce") {
+
+    rr <- fselect_nested(
+                method = method,
+                task = task,
+                learner = learner,
+                inner_resampling = rsmp("holdout"),
+                outer_resampling = rsmp("cv", folds = 10),
+                measure = msr(measure),
+                term_evals = nevals)
+
+    return(rr)
+}
